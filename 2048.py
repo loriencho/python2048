fccd472a124cc51415afef4347db436d06857a62
Lorien Cho <lorien.yuna.cho@gmail.com>
2023-03-21 13:41:25 -0400

    changed board display, fixed issues with combine_tiles

--- 2048.py
+++ 2048.py
@@ -1,8 +1,13 @@
+import copy
 import random 
 # Students will be using
 def display_board(board):
     for row in board:
-        print(row) 
+        print("[", end="")
+        for item in row:
+            print(' %3d ' % item, end = "") 
+        print("]")
+
     print()
 
 # Students will be using
@@ -27,46 +32,53 @@ def create_initial_board():
 
 # Students will be using
 def push(direction, board):  
-    return push_recurse(direction, board, create_initial_board())
+    board = push_recurse(direction, board, create_initial_board(), "compress")
+    board = push_recurse(direction, board, create_initial_board(), "merge")
+    return push_recurse(direction, board, create_initial_board(), "compress")
+
 
 # INTERNAL FUNCTION (RECURSIVE)
-def push_recurse(direction, current_board, past_board):
+def push_recurse(direction, current_board, past_board, mode):
     if(current_board == past_board): # push in that direction until we can't anymore
         return current_board
     else:
-        past_board = current_board.copy()
+        past_board = copy.deepcopy(current_board)
         if(direction == "left"):
             for row_index in range(0, 4, 1):
                 for col_index in range(0, 3, 1):
-                    # store old board in a new variable called old_board here
-                    combineTiles(current_board, (row_index, col_index), (row_index, col_index + 1))
+                    combine_tiles(current_board, (row_index, col_index), (row_index, col_index + 1), mode)
         elif(direction == "right"):
             for row_index in range(0, 4, 1):
                 for col_index in range(3, 0, -1):
-                    combineTiles(current_board, (row_index, col_index), (row_index, col_index - 1))
+                    combine_tiles(current_board, (row_index, col_index), (row_index, col_index - 1), mode)
         elif (direction=="up"):
             for row_index in range(1, 4, 1):
                 for col_index in range(0, 4, 1):
-                    combineTiles(current_board, (row_index - 1, col_index), (row_index, col_index))
+                    combine_tiles(current_board, (row_index - 1, col_index), (row_index, col_index), mode)
+
         elif (direction=="down"):
-            for row_index in range(2, 0, -1):
+            for row_index in range(2, -1, -1):
                 for col_index in range(0, 4, 1):
-                    combineTiles(current_board, (row_index + 1, col_index), (row_index, col_index))               
-        push_recurse(direction, current_board, past_board)
+                    combine_tiles(current_board, (row_index + 1, col_index), (row_index, col_index), mode)
+        
+        push_recurse(direction, current_board, past_board, mode)
 
     return board
 
 # INTERNAL FUNCTION
-def combineTiles(board, current_tile, pushed_tile):
+def combine_tiles(board, current_tile, pushed_tile, mode):
     current_tile_value = board[current_tile[0]][current_tile[1]]
     pushed_tile_value = board[pushed_tile[0]][pushed_tile[1]]
-    if (current_tile_value == pushed_tile_value):
-        board[current_tile[0]][current_tile[1]] = current_tile_value * 2
-        board[pushed_tile[0]][pushed_tile[1]] = 0
 
-    if (current_tile_value == 0 and pushed_tile_value!=0):
-        board[current_tile[0]][current_tile[1]] = pushed_tile_value
-        board[pushed_tile[0]][pushed_tile[1]] = 0
+    if mode == "merge":
+        if (current_tile_value == pushed_tile_value 
+            and current_tile_value!=0):
+            board[current_tile[0]][current_tile[1]] = current_tile_value * 2
+            board[pushed_tile[0]][pushed_tile[1]] = 0
+    if mode == "compress":  
+        if (current_tile_value == 0 and pushed_tile_value!=0):
+            board[current_tile[0]][current_tile[1]] = pushed_tile_value
+            board[pushed_tile[0]][pushed_tile[1]] = 0
     
 # INTERNAL FUNCTION
 def return_empty_tiles(board):
@@ -83,7 +95,11 @@ insert_random_tile(board)
 while(insert_random_tile(board) != False):
     print()
     display_board(board)
+
     move = input("Use w,a,s,d to pick a direction to move")
+    while move not in ("w", "a", "s", "d"):
+        print("Error. Please re-enter a proper direction")
+        move = input("Use w,a,s,d to pick a direction to move")
     if(move == "w"):
         push("up", board)
     elif(move == "a"):
